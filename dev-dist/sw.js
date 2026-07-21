f711218e7955c3071506b0629b63ea313dbd0dbe
forgeindiaconnect-Techies <forgeindiaconnectfic@gmail.com>
2026-07-21 17:53:30 +0530

    Update service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.m8k2sqso1uo"
+    "revision": "0.ubbu5p32vn"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
