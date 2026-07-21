c19f85d14afb1508ea73eb40443ad2dc046f0216
forgeindiaconnect-Techies <forgeindiaconnectfic@gmail.com>
2026-07-21 14:03:31 +0530

    fix: Support FIREBASE_SERVICE_ACCOUNT env var for production

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -1,5 +1,15 @@
 const { initializeApp, cert } = require("firebase-admin/app");
-const serviceAccount = require("./firebase-service-account.json");
+
+let serviceAccount;
+try {
+  serviceAccount = require("./firebase-service-account.json");
+} catch (err) {
+  if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  } else {
+    console.error("Missing FIREBASE_SERVICE_ACCOUNT environment variable");
+  }
+}
 
 const app = initializeApp({
   credential: cert(serviceAccount),
